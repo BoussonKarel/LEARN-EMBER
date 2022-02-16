406c236c530ccb59726205dcafac239f4fbe4141
Karel <karel.bousson@student.howest.be>
2022-02-16 22:41:02 +0100

    other syntax

--- super-rentals/app/components/rental/image.js
+++ super-rentals/app/components/rental/image.js
@@ -1,8 +1,9 @@
 import Component from '@glimmer/component';
 
 export default class RentalImageComponent extends Component {
-  constructor(...args) {
-    super(...args);
-    this.isLarge = false;
-  }
+  // constructor(...args) {
+  //   super(...args);
+  //   this.isLarge = false;
+  // }
+  isLarge = false
 }
